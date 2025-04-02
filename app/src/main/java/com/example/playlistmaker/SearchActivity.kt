cd21e32c98da6e59bf5b96a4300a30306d73959e
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2025-04-02 17:10:27 +0700

    sprint 9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,12 +1,35 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+
+const val SEARCH_TEXT = "search_text"
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchText: String = ""
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.putString(SEARCH_TEXT, searchText).toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +39,51 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT).toString()
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        val inputEditLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
+        val inputEditText = findViewById<EditText>(R.id.searchInput)
+
+        inputEditLayout.isEndIconVisible = false
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                inputEditLayout.isEndIconVisible = !s.isNullOrEmpty()
+                searchText = s.toString()
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+
+        inputEditLayout.setEndIconOnClickListener {
+            inputEditText.text?.clear()
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,24 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import androidx.activity.addCallback
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SettingsActivity : AppCompatActivity() {
+
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,88 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        val btnSearch = findViewById<LinearLayout>(R.id.share)
+        val btnMedia = findViewById<LinearLayout>(R.id.support)
+        val btnSetting = findViewById<LinearLayout>(R.id.agreement)
+
+        btnSearch.setOnClickListener(this@SettingsActivity)
+        btnMedia.setOnClickListener(this@SettingsActivity)
+        btnSetting.setOnClickListener(this@SettingsActivity)
+
+        val switch = findViewById<SwitchCompat>(R.id.switch1)
+
+        switch.setChecked(isDarkThemeEnabled());
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            setAppTheme(isChecked)
+        }
+    }
+
+    private fun setAppTheme(isDarkModeOn: Boolean) {
+        if (isDarkModeOn) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun isDarkThemeEnabled(): Boolean {
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        return nightMode == AppCompatDelegate.MODE_NIGHT_YES
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.setType("text/plain")
+                val link = getString(R.string.yandex_link)
+                shareIntent.putExtra(Intent.EXTRA_TEXT, link)
+                startActivity(Intent.createChooser(shareIntent, getString(R.string.share_link)))
+            }
+
+            R.id.support -> {
+                val recipientEmail = getString(R.string.mail_student)
+
+                val mailUri = Uri.parse("mailto:$recipientEmail")
+                    .buildUpon()
+                    .appendQueryParameter(
+                        getString(R.string.subject),
+                        getString(R.string.subject_title)
+                    )
+                    .appendQueryParameter(
+                        getString(R.string.body),
+                        getString(R.string.body_title)
+                    )
+                    .build()
+
+                val emailIntent = Intent(Intent.ACTION_SENDTO, mailUri)
+                startActivity(emailIntent)
+            }
+
+            R.id.agreement -> {
+                val url = getString(R.string.offer)
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(browserIntent)
+            }
         }
     }
 }

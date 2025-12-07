3527b6dceae8c617aafd7e9dab09edbd7d60351c
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2025-12-07 15:48:49 +0700

    sprint 27, level 1

--- app/src/main/java/com/example/playlistmaker/track/TrackActivity.kt
+++ app/src/main/java/com/example/playlistmaker/track/TrackActivity.kt
@@ -11,52 +11,16 @@ import android.os.IBinder
 import android.view.MenuItem
 import android.view.View.GONE
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import coil.compose.AsyncImage
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.core.service.MusicService
-import com.example.playlistmaker.core.theme.AppTextStyle
-import com.example.playlistmaker.core.theme.toTextStyle
 import com.example.playlistmaker.databinding.ActivityTrackBinding
 import com.example.playlistmaker.searchMusic.domain.models.Track
 import com.example.playlistmaker.searchMusic.presentation.TRACK
@@ -68,168 +32,6 @@ import java.time.Instant
 import java.time.ZoneId
 import java.util.Locale
 
-//
-//class TrackActivity : ComponentActivity() {
-//    private val trackViewModel: TrackViewModel by viewModel {
-//        parametersOf(track)
-//    }
-//    //private lateinit var bottomSheetController: BottomSheetController
-//
-//    private val track: Track by lazy {
-//        intent.getParcelableExtra(TRACK)!!
-//    }
-//
-//    private val serviceConnection = object : ServiceConnection {
-//        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-//            val binder = service as MusicService.MusicServiceBinder
-//            trackViewModel.setAudioPlayerControl(binder.getService())
-//        }
-//
-//        override fun onServiceDisconnected(name: ComponentName?) {
-//            trackViewModel.removeAudioPlayerControl()
-//        }
-//    }
-//
-//    @OptIn(ExperimentalMaterial3Api::class)
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContent {
-//
-//            Scaffold(
-//                topBar = {
-//                    TopAppBar(
-//
-//                        title = { Text("") },
-//                        colors = TopAppBarDefaults.topAppBarColors(
-//                            containerColor = Color.White,
-//                            titleContentColor = Color.White
-//                        )
-//                    )
-//
-//                },
-//                modifier = Modifier.fillMaxSize(),
-//
-//                ) { paddingValues ->
-//
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .padding(paddingValues)
-//                        .verticalScroll(rememberScrollState())
-//                ) {
-//                    TrackContent(track = track)
-//                }
-//            }
-//
-//
-//        }
-//    }
-//
-//    @Composable
-//    private fun TrackContent(track: Track) {
-//        val durationTrack =
-//            SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
-//
-//        Column {
-//            // ImageViewer(track.previewUrl)
-//            Text(
-//                text = track.trackName,
-//                style = TextStyle(
-//                    fontWeight = FontWeight.W700,
-//                    fontSize = 20.sp,
-//                    color = Color.Black,
-//                )
-//            )
-//            Spacer(modifier = Modifier.height(12.dp))
-//            Text(text = track.artistName)
-//            Spacer(modifier = Modifier.height(30.dp))
-//            Row(
-//                modifier = Modifier.fillMaxWidth(),
-//                horizontalArrangement = Arrangement.SpaceBetween,
-//                verticalAlignment = Alignment.CenterVertically,
-//            ) {
-//
-//                Image(
-//                    painter = painterResource(id = R.drawable.add_album),
-//                    contentDescription = null,
-//                    modifier = Modifier
-//                        .clickable {
-//
-//                        }
-//                        .size(51.dp)
-//                )
-//                Image(
-//                    painter = painterResource(id = R.drawable.play),
-//                    contentDescription = null,
-//                    modifier = Modifier
-//                        .clickable {
-//
-//                        }
-//                        .size(83.dp)
-//                )
-//                Image(
-//                    painter = painterResource(id = R.drawable.favorite_disabled),
-//                    contentDescription = null,
-//                    modifier = Modifier
-//                        .clickable {
-//
-//                        }
-//                        .size(51.dp)
-//                )
-//            }
-//            Spacer(modifier = Modifier.height(12.dp))
-//            Text(
-//                "00:00", textAlign = TextAlign.Center,
-//                modifier = Modifier.fillMaxWidth()
-//            )
-//            Spacer(modifier = Modifier.height(30.dp))
-//            TrackInfo(R.string.duration, durationTrack)
-//            if (track.collectionName != null) {
-//                Spacer(modifier = Modifier.height(24.dp))
-//                TrackInfo(R.string.album, track.collectionName)
-//            }
-//            if (track.releaseDate != null) {
-//                val instant = Instant.parse(track.releaseDate)
-//                val zonedDateTime = instant.atZone(ZoneId.of("UTC"))
-//
-//                Spacer(modifier = Modifier.height(24.dp))
-//                TrackInfo(R.string.year, zonedDateTime.year.toString())
-//            }
-//            if (track.primaryGenreName != null) {
-//                Spacer(modifier = Modifier.height(24.dp))
-//                TrackInfo(R.string.genre, track.primaryGenreName)
-//            }
-//            if (track.country != null) {
-//                Spacer(modifier = Modifier.height(24.dp))
-//                TrackInfo(R.string.country, track.country)
-//            }
-//        }
-//    }
-//
-//    @Composable
-//    fun TrackInfo(resId: Int, value: String) {
-//        val isDark = isSystemInDarkTheme()
-//        Row(
-//            modifier = Modifier.fillMaxWidth(),
-//            horizontalArrangement = Arrangement.SpaceBetween
-//        ) {
-//            Text(stringResource(id = resId),style= TextStyle())
-//            Text(value)
-//
-//        }
-//    }
-//
-//    @Composable
-//    fun ImageViewer(imageUrl: String?) {
-//        AsyncImage(
-//            model = imageUrl,
-//            contentDescription = null,
-//            modifier = Modifier.fillMaxSize(),
-//            contentScale = ContentScale.Fit
-//        )
-//    }
-//}
 class TrackActivity : AppCompatActivity() {
     private val trackViewModel: TrackViewModel by viewModel {
         parametersOf(track)
